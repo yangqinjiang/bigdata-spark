c7130f1468b0f26a3eebdf01f02893937de0a481
yangqinjiang <531553280@qq.com>
2021-07-13 14:21:55 +0800

    使用DSL方式计算广告投放报表

--- src/main/scala/offline/report/AdsRegionAnalysisReport.scala
+++ src/main/scala/offline/report/AdsRegionAnalysisReport.scala
@@ -1,6 +1,7 @@
 package offline.report
 
 import offline.config.ApplicationConfig
+import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 /**
@@ -14,7 +15,7 @@ object AdsRegionAnalysisReport {
   def reportWithKpiSql(dataframe: DataFrame): DataFrame = {
 
     //从DataFrame中获取SparkSession对象
-    val spark:SparkSession = dataframe.sparkSession
+    val spark: SparkSession = dataframe.sparkSession
     /*
     在SparkSQL中使用SQL分析数据时，步骤分为两步：
     - 第一步、将DataFrame注册为临时视图
@@ -33,6 +34,7 @@ object AdsRegionAnalysisReport {
 
   /**
    * 使用SQL 方式计算广告投放报表
+   *
    * @param dataframe
    * @return
    */
@@ -53,7 +55,7 @@ object AdsRegionAnalysisReport {
       ReportSQLConstant.reportAdsRegionSQL(tmp_view_pmt)
     )
     reportDF.printSchema()
-    reportDF.show(20,truncate = false)
+    reportDF.show(20, truncate = false)
 
     //iii.为了计算"三率"首先注册DataFrame为临时视图
     reportDF.createOrReplaceTempView("tmp_view_report")
@@ -65,7 +67,7 @@ object AdsRegionAnalysisReport {
     )
 
     resultDF.printSchema()
-    resultDF.show(20,truncate = false)
+    resultDF.show(20, truncate = false)
 
     //返回结果
     resultDF
@@ -76,7 +78,7 @@ object AdsRegionAnalysisReport {
    */
   def saveResultToMySQL(dataframe: DataFrame) = {
     dataframe.coalesce(1)
-      .write.mode(SaveMode.Append)// 设置MySQL数据库相关属性
+      .write.mode(SaveMode.Append) // 设置MySQL数据库相关属性
       .format("jdbc")
       .option("driver", ApplicationConfig.MYSQL_JDBC_DRIVER)
       .option("url", ApplicationConfig.MYSQL_JDBC_URL)
@@ -86,23 +88,154 @@ object AdsRegionAnalysisReport {
       .save()
   }
 
+  /**
+   * 使用DSL方式计算广告投放报表
+   */
+  def reportWithDql(dataframe: DataFrame): DataFrame = {
+    // i. 导入隐式转换及函数库
+
+    import dataframe.sparkSession.implicits._
+    import org.apache.spark.sql.functions._
+
+    // ii 报表开发
+    dataframe
+      //第一步,按照维度分组,省份和城市
+      .groupBy($"province", $"city")
+      //第二步,使用agg进行聚合操作,主要使用CASE...WHEN...函数和SUM函数
+      .agg(
+        //原始请求: requestmode = 1 and processnode >= 1
+        sum(
+          when($"requestmode".equalTo(1).and($"processnode".geq(1)), 1).otherwise(0),
+        ).as("orginal_req_cnt"),
+        // 有效请求：requestmode = 1 and processnode >= 2
+        sum(
+          when($"requestmode".equalTo(1)
+            .and($"processnode".geq(2)), 1
+          ).otherwise(0)
+        ).as("valid_req_cnt"),
+        // 广告请求：requestmode = 1 and processnode = 3
+        sum(
+          when($"requestmode".equalTo(1)
+            .and($"processnode".equalTo(3)), 1
+          ).otherwise(0)
+        ).as("ad_req_cnt"),
+        // 参与竞价数
+        sum(
+          when($"adplatformproviderid".geq(100000)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"isbid".equalTo(1))
+            .and($"adorderid".notEqual(0)), 1
+          ).otherwise(0)
+        ).as("join_rtx_cnt"),
+        // 竞价成功数
+        sum(
+          when($"adplatformproviderid".geq(100000)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"iswin".equalTo(1))
+            .and($"adorderid".notEqual(0)), 1
+          ).otherwise(0)
+        ).as("success_rtx_cnt"),
+        // 广告主展示数: requestmode = 2 and iseffective = 1
+        sum(
+          when($"requestmode".equalTo(2)
+            .and($"iseffective".equalTo(1)), 1
+          ).otherwise(0)
+        ).as("ad_show_cnt"),
+        // 广告主点击数: requestmode = 3 and iseffective = 1 and adorderid != 0
+        sum(
+          when($"requestmode".equalTo(3)
+            .and($"iseffective".equalTo(1))
+            .and($"adorderid".notEqual(0)), 1
+          ).otherwise(0)
+        ).as("ad_click_cnt"),
+        // 媒介展示数
+        sum(
+          when($"requestmode".equalTo(2)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"isbid".equalTo(1))
+            .and($"iswin".equalTo(1)), 1
+          ).otherwise(0)
+        ).as("media_show_cnt"),
+        // 媒介点击数
+        sum(
+          when($"requestmode".equalTo(3)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"isbid".equalTo(1))
+            .and($"iswin".equalTo(1)), 1
+          ).otherwise(0)
+        ).as("media_click_cnt"),
+        // DSP 广告消费
+        sum(
+          when($"adplatformproviderid".geq(100000)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"iswin".equalTo(1))
+            .and($"adorderid".gt(200000))
+            .and($"adcreativeid".gt(200000)), floor($"winprice" / 1000)) otherwise (0)
+        ).as("dsp_pay_money"),
+        // DSP广告成本
+        sum(
+          when($"adplatformproviderid".geq(100000)
+            .and($"iseffective".equalTo(1))
+            .and($"isbilling".equalTo(1))
+            .and($"isbid".equalTo(1))
+            .and($"iswin".equalTo(1))
+            .and($"adorderid".gt(200000))
+            .and($"adcreativeid".gt(200000)), floor($"adpayment" / 1000)) otherwise (0)
+        ).as("dsp_cost_money"))
+      // 第三步、过滤非0数据
+      .filter(
+        $"join_rtx_cnt".notEqual(0)
+          .and($"success_rtx_cnt".notEqual(0))
+          .and($"ad_show_cnt".notEqual(0))
+          .and($"ad_click_cnt".notEqual(0))
+          .and($"media_show_cnt".notEqual(0))
+          .and($"media_click_cnt".notEqual(0))
+      )
+      // 第四步、计算“三率”, 增加三列数据
+      .withColumn(
+        "success_rtx_rate", //
+        round($"success_rtx_cnt" / $"join_rtx_cnt", 2) // 保留两位有效数字
+      )
+      .withColumn(
+        "ad_click_rate", //
+        round($"ad_click_cnt" / $"ad_show_cnt", 2) // 保留两位有效数字
+      )
+      .withColumn(
+        "media_click_rate", //
+        round($"media_click_cnt" / $"media_show_cnt", 2) // 保留两位有效数字
+      )
+      // 第五步、增加报表的日期
+      .withColumn(
+        "report_date", // 报表日期字段
+        date_sub(current_date(), 1).cast(StringType)
+      )
+    // iii. 返回结果数据
+    dataframe
+    
+  }
+
   /*
-        不同业务报表统计分析时，两步骤：
-        i. 编写SQL或者DSL分析
-        ii. 将分析结果保存MySQL数据库表中
+          不同业务报表统计分析时，两步骤：
+          i. 编写SQL或者DSL分析
+          ii. 将分析结果保存MySQL数据库表中
 
 
-        */
+          */
   def doReport(dataframe: DataFrame) = {
 
     //第一,计算报表
-   //上述SQL使用子查询方式，需要两次注册DataFrame为临时视图，编写SQL语句，可以使用With As语句优化。
-//    val resultDF:DataFrame = reportWithSql(dataframe)//sql编程
-    val resultDF:DataFrame = reportWithKpiSql(dataframe)//sql编程
-//    val resultDF:DataFrame = reportWithDql(dataframe)//dsl编程
+    //上述SQL使用子查询方式，需要两次注册DataFrame为临时视图，编写SQL语句，可以使用With As语句优化。
+    //    val resultDF:DataFrame = reportWithSql(dataframe)//sql编程
+    //    val resultDF:DataFrame = reportWithKpiSql(dataframe)//sql编程
+    val resultDF: DataFrame = reportWithDql(dataframe) //dsl编程
 
     // 第二,保存数据
-    resultDF.show(20,truncate = false)
+    resultDF.show(20, truncate = false)
     saveResultToMySQL(resultDF)
   }
 }
