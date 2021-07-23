583362442142edc0ae887c5445a2daba34100bf6
yangqinjiang <531553280@qq.com>
2021-07-23 12:32:26 +0800

    spark es

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,7 @@
-
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import rt.config.ApplicationConfig
 
 object Test {
 
@@ -14,8 +17,53 @@ object Test {
 //    val javaList: util.List[Int] = ArrayBuffer(1,2,3).asJava
 //    println(javaList)
 
-    var holder = "?"
-    holder = "?" * 3
-    println(holder.split("").mkString(",").toString)
+//    var holder = "?"
+//    holder = "?" * 3
+//    println(holder.split("").mkString(",").toString)
+
+    val spark = SparkSession.builder().appName(this.getClass.getSimpleName.stripSuffix("$"))
+      .master("local[2]")
+      //设置shuffle分区数目
+      .config("spark.sql.shuffle.partitions", "2")
+      .config("spark.testing.memory","471859200")
+      .getOrCreate()
+
+    //TODO: 导入隐式转换和函数库
+    import spark.implicits._
+    //    import org.apache.spark.sql.functions._
+
+    // 从文件系统,监控目录,读取csv格式数据
+    // 数据样本->   jack;23;running
+
+    //定义schema
+    val csvSchema: StructType = new StructType()
+      .add("name", StringType, nullable = true)
+      .add("age", IntegerType, nullable = true)
+      .add("hobby", StringType, nullable = true)
+
+    val inputStreamDF: DataFrame = spark.readStream.option("sep", ";")
+      .option("header", "false")
+      .schema(csvSchema) // 指定schema信息
+      .csv("file:///E:/csvdatas/")
+
+    //依据业务需求,分析数据,统计年龄小于25岁的人群的爱好排行榜
+    val resultStreamDF: Dataset[Row] = inputStreamDF
+      //年龄小于25岁
+      .filter($"age" < 25)
+      .select("*")
+
+    //设置streaming应用输出及启动
+    val query = resultStreamDF.writeStream.outputMode(OutputMode.Append())
+      .option("checkpointLocation", "datas/order-apps/ckpt/test-ckpt/")
+      .format("es")
+      .option("es.nodes", ApplicationConfig.ES_NODES)
+      .option("es.port", ApplicationConfig.ES_PORT)
+      .option("es.index.auto.create", ApplicationConfig.ES_INDEX_AUTO_CREATE)
+      .option("es.write.operation", ApplicationConfig.ES_WRITE_OPERATION)
+      .option("es.mapping.id", ApplicationConfig.ES_MAPPING_ID)
+      .start(ApplicationConfig.ES_INDEX_NAME)
+    // 查询器等待流式应用终止
+    query.awaitTermination()
+    query.stop() // 等待所有任务运行完成才停止运行
   }
 }
---
+++ src/main/scala/rt/store/es/RealTimeOrder2Es.scala
@@ -0,0 +1,65 @@
+package rt.store.es
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.{StringType, StructType}
+import rt.config.ApplicationConfig
+import rt.utils.{SparkUtils, StreamingUtils}
+
+/**
+ * StructuredStreaming 实时消费Kafka Topic中数据，存入到Elasticsearch索引中
+ */
+object RealTimeOrder2Es {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkUtils.createSparkSession(this.getClass)
+    import org.apache.spark.sql.functions._
+    import spark.implicits._
+    //2,从kafka读取消费数据
+    val kafkaStreamDF: DataFrame = spark.readStream.format("kafka")
+      .option("kafka.bootstrap.servers", ApplicationConfig.KAFKA_BOOTSTRAP_SERVERS)
+      .option("subscribe", ApplicationConfig.KAFKA_ETL_TOPIC)
+      //从Kafka消费数据时，通过属性【maxOffsetsPerTrigger】，设置每批次最大数据量，实
+      //际生产项目需要结合流式数据波峰及应用运行资源综合考虑设置；
+      .option("maxOffsetsPerTrigger", ApplicationConfig.KAFKA_MAX_OFFSETS)
+      .load()
+
+    //schema信息
+    // {"orderId":"20200519161403348000001","userId":"200000291","orderTime":"2020-05-19 16:14:03
+    // .348","ip":"222.68.105.201","orderMoney":"477.22","orderStatus":0,"province":"上海","city":"上海市"}
+    val orderSchema: StructType = new StructType()
+      .add("orderId",StringType,nullable = true)
+      .add("userId",StringType,nullable = true)
+      .add("orderTime",StringType,nullable = true)
+      .add("ip",StringType,nullable = true)
+      .add("orderMoney",StringType,nullable = true)
+      .add("orderStatus",StringType,nullable = true)
+      .add("province",StringType,nullable = true)
+      .add("city",StringType,nullable = true)
+
+    //3 获取订单记录数据
+    val orderStreamDF = kafkaStreamDF
+      //获取value字段的值, 转换为String类型
+      .selectExpr("CAST(value AS STRING)")
+      .as[String]
+      .filter(line => null != line && line.trim.length > 0)
+      .select(from_json($"value", orderSchema).as("order"))
+      .select($"order.*") // 从上面的select获取order字段
+
+    orderStreamDF.printSchema()
+
+    //4输出数据到ElasticSearch和启动流式应用
+    val query = orderStreamDF.writeStream.outputMode(OutputMode.Append())
+      .option("checkpointLocation", ApplicationConfig.STREAMING_ES_CKPT)
+      .format("es")
+      .option("es.nodes", ApplicationConfig.ES_NODES)
+      .option("es.port", ApplicationConfig.ES_PORT)
+      .option("es.index.auto.create", ApplicationConfig.ES_INDEX_AUTO_CREATE)
+      .option("es.write.operation", ApplicationConfig.ES_WRITE_OPERATION)
+      .option("es.mapping.id", ApplicationConfig.ES_MAPPING_ID)
+      .start(ApplicationConfig.ES_INDEX_NAME)
+
+    StreamingUtils.stopStructuredStreaming(query,ApplicationConfig.STOP_ES_FILE)
+
+  }
+}
